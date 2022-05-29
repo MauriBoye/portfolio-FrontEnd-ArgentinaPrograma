2b93ef327ff7c32ca32854aae12b031ba54e60eb
Mauri <mauriboye@gmail.com>
2022-05-28 21:25:37 -0300

    Add education component

--- AngularWebPrototype/src/app/components/education/education.component.ts
+++ AngularWebPrototype/src/app/components/education/education.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JsonService } from '../../json.service';
 
 @Component({
   selector: 'app-education',
@@ -7,9 +8,58 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EducationComponent implements OnInit {
 
-  constructor() { }
+  data: any  = [];
+  add=false;
+  edit=false;
+  idEdit=0;
+
+  updateEducation(){
+    const body = {
+      id: this.idEdit,
+      institute: (<HTMLInputElement>document.getElementById("institute")).value,
+      career: (<HTMLInputElement>document.getElementById("career")).value,
+      entryDate: (<HTMLInputElement>document.getElementById("entryDate")).value,
+      exitDate: (<HTMLInputElement>document.getElementById("exitDate")).value,
+      description: (<HTMLInputElement>document.getElementById("description")).value,
+      logoUrl: (<HTMLInputElement>document.getElementById("logoUrl")).value===""?"https://dummyimage.com/400x400/6e6e6e/ffffff.jpg&text=+LOGO":(<HTMLInputElement>document.getElementById("logoUrl")).value
+      ,
+      person: {
+          id: 1
+      }
+  }
+    this.edit=false;
+    this.json.updateJson('http://localhost:8080/api/education/update', body).subscribe(res=>{
+      this.ngOnInit();
+    })
+  }
+  addEducation(){
+    const body = {
+      institute: (<HTMLInputElement>document.getElementById("institute")).value,
+      career: (<HTMLInputElement>document.getElementById("career")).value,
+      entryDate: (<HTMLInputElement>document.getElementById("entryDate")).value,
+      exitDate: (<HTMLInputElement>document.getElementById("exitDate")).value,
+      description: (<HTMLInputElement>document.getElementById("description")).value,
+      logoUrl: (<HTMLInputElement>document.getElementById("logoUrl")).value===""?"https://dummyimage.com/400x400/6e6e6e/ffffff.jpg&text=+LOGO":(<HTMLInputElement>document.getElementById("logoUrl")).value,
+      person: {
+          id: 1
+      }
+  }
+    this.add=false;
+    this.json.postJson('http://localhost:8080/api/education/create', body).subscribe(res=>{
+      this.ngOnInit();
+    })
+  }
+  deleteEducation(id:any){
+    this.json.deleteJson('http://localhost:8080/api/education/delete/' + id).subscribe(res=>{
+      this.ngOnInit();
+    })
+  }
+  
+  constructor(public json:JsonService) { }
 
   ngOnInit(): void {
+    this.json.getJson('http://localhost:8080/api/education/list').subscribe((res:any)=>{
+      this.data = res
+    })  
   }
-
 }
